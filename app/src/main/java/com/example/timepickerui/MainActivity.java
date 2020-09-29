5469a3b553a969e959c01eeb41771737c2ab0001
sweetydubey <sweetydubey39@gmail.com>
2020-09-29 12:44:07 +0530

    first commit

---
+++ app/src/main/java/com/example/timepickerui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.timepickerui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Smart Home");
+    }
+}
